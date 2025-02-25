f2d0fd96a82ccbfe8498d4035c2adadbb4b6eaf1
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-02-25 15:14:48 -0300

    chore: add dotenv

--- src/main/java/com/acc/event/EventApplication.java
+++ src/main/java/com/acc/event/EventApplication.java
@@ -3,10 +3,15 @@ package com.acc.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EventApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(EventApplication.class, args);
 	}
 
